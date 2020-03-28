fd0deeb7a720dc93002cb1c6968b1b1d5296c2ba
DanielNetzer <daniel.netzer@vonage.com>
2020-03-28 13:06:22 +0300

    chore(scrape): starting library from scratch

--- projects/ngx-diagrams/src/lib/defaults/label/default-label.component.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultLabelModel } from './default-label.model';
-
-export interface DefaultLabelWidgetProps {
-	model: DefaultLabelModel;
-}
-
-export class DefaultLabelComponent {
-	model: DefaultLabelModel;
-}
