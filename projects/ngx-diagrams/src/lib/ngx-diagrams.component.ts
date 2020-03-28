3f0de50a4a70b9c75e10cb5d81c86ec53dd971d5
DanielNetzer <daniel.netzer@vonage.com>
2020-03-28 13:08:54 +0300

    chore(library): new library

---
+++ projects/ngx-diagrams/src/lib/ngx-diagrams.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'lib-ngx-diagrams',
+	template: `
+		<p>
+			ngx-diagrams works!
+		</p>
+	`,
+	styles: []
+})
+export class NgxDiagramsComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit(): void {}
+}
---
+++ projects/ngx-diagrams/src/lib/ngx-diagrams.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { NgxDiagramsComponent } from './ngx-diagrams.component';
+
+@NgModule({
+	declarations: [NgxDiagramsComponent],
+	imports: [],
+	exports: [NgxDiagramsComponent]
+})
+export class NgxDiagramsModule {}
